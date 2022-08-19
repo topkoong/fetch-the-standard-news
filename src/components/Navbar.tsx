a475da2c6a47946ff86f3ec81fb98104ed794b73
toppy <theerut@shoplive.dev>
2022-08-20 00:02:15 +0700

    Tested spa route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,9 +16,9 @@ function NavBar() {
     <nav className="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700">
       <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
         <div className="flex items-center flex-shrink-0 text-gray-800 mr-16">
-          <span className="font-semibold text-3xl tracking-tight uppercase">
+          <Link to="/" className="font-semibold text-3xl tracking-tight uppercase">
             Top News
-          </span>
+          </Link>
         </div>
         <div className="block lg:hidden">
           <button
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fetch-the-standard-news/',
+  base: '/fetch-the-standard-news',
   plugins: [preact()],
 });
