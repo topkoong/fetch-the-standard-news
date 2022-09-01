2dbe1ea91eaca8c8c9a3c172315af7b54107b080
toppy <theerut@shoplive.dev>
2022-09-01 22:32:05 +0700

    Fixed preact module error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'preact/hooks';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   const categories: { [key: string]: string } = {
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import preact from '@preact/preset-vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
