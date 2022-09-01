0ce3e6fca2ed74f94495fa96b3d5d5d0cad1728e
toppy <theerut@shoplive.dev>
2022-09-01 21:13:13 +0700

    Updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'preact/hooks';
 import { Link } from 'react-router-dom';
+import { useState } from 'preact/hooks';
 
 function NavBar() {
   const categories: { [key: string]: string } = {
@@ -19,7 +19,7 @@ function NavBar() {
       <div className='flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0'>
         <div className='flex items-center flex-shrink-0 text-gray-800 mr-16'>
           <Link to='/' className='font-semibold text-3xl tracking-tight uppercase'>
-            Top News
+            Lazy News
           </Link>
         </div>
         <div className='block lg:hidden'>
@@ -29,7 +29,7 @@ function NavBar() {
             className='flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700'
             onClick={() => setIsNavbarOpen(!isNavbarOpen)}
           >
-            {isNavbarOpen ? (
+            {!isNavbarOpen ? (
               <svg
                 className='fill-current h-3 w-3'
                 viewBox='0 0 20 20'
@@ -40,9 +40,8 @@ function NavBar() {
               </svg>
             ) : (
               <svg
-                className='block h-3 w-3 lg:hidden'
+                className='fill-current h-3 w-3 lg:hidden'
                 xmlns='http://www.w3.org/2000/svg'
-                fill='none'
                 viewBox='0 0 24 24'
                 stroke='currentColor'
                 aria-hidden='true'
@@ -50,7 +49,7 @@ function NavBar() {
                 <path
                   strokeLinecap='round'
                   strokeLinejoin='round'
-                  strokeWidth='2'
+                  strokeWidth='3'
                   d='M6 18L18 6M6 6l12 12'
                 />
               </svg>
