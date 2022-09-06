8168b1ae31dffe2de18af18c3e940e548b4ed0ed
toppy <theerut@shoplive.dev>
2022-09-07 00:43:59 +0700

    Updated posts per page fetching

--- src/apis/posts.ts
+++ src/apis/posts.ts
@@ -2,7 +2,7 @@ import { THE_STANDARD_POSTS_ENDPOINT } from '@constants/index';
 import axios from 'axios';
 
 const fetchPosts = async (): Promise<any> => {
-  const { data } = await axios.get(`${THE_STANDARD_POSTS_ENDPOINT}?per_page=60`);
+  const { data } = await axios.get(`${THE_STANDARD_POSTS_ENDPOINT}?per_page=100`);
   return data;
 };
 
