72128c604f594ea32e5e8f819af7d5eb346af83b
toppy <theerut@shoplive.dev>
2022-09-07 14:42:02 +0700

    Updated cache script

---
+++ cachescripts/fetch-the-standard-categories.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Scrape post categories from the standard and store JSON data on disk
+workdir="cachescripts"
+categoriesBaseUrl="https://thestandard.co/wp-json/wp/v2/categories"
+queryPerPage=100
+categoriesDir="${workdir}/categories-json"
+categoriesGroupFilename="category-group"
+outputDir="${categoriesDir}/merged"
+outputFilename="categories.json"
+cachedDir="src/assets/cached"
+
+getTotalCategoryPages() {
+    echo "Fetching the standard categories"
+    headers=$(curl -I "https://thestandard.co/wp-json/wp/v2/categories?page=1&per_page=${queryPerPage}")
+    rawTotalPosts=$(echo "$headers" | grep -Fi X-WP-Total:)
+    rawTotalPages=$(echo "$headers" | grep -Fi X-WP-TotalPages:)
+    totalPosts=${rawTotalPosts//[!0-9]/}
+    totalPages=${rawTotalPages//[!0-9]/}
+    echo "totalPages: $totalPages"
+    echo "totalPosts: $totalPosts"
+    echo "queryPerPage: $queryPerPage"
+    for ((count = 1; count <= $totalPages; count++)); do
+        if [ $count -eq $totalPages ]; then
+            quriedPages=$((count - 1))
+            totalQueriedPages=$((queryPerPage * quriedPages))
+            remainingPosts=$((totalPosts - totalQueriedPages))
+            echo "remainingPosts: $remainingPosts"
+            echo "Fetching the standard categories page: $count"
+            curl -s "${categoriesBaseUrl}?page=${count}&per_page=${remainingPosts}" \
+                -H 'Accept: application/json' \
+                -H 'Content-Type: application/json' |
+                jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
+        else
+            echo "Fetching the standard categories page: $count"
+            curl -s "${categoriesBaseUrl}?page=${count}&per_page=${queryPerPage}" \
+                -H 'Accept: application/json' \
+                -H 'Content-Type: application/json' |
+                jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
+        fi
+    done
+}
+mergeJsonFiles() {
+    jq -s 'flatten' ./${categoriesDir}/${categoriesGroupFilename}-*.json >./${outputDir}/${outputFilename}
+    mv ./${outputDir}/${outputFilename} ${cachedDir}/${outputFilename}
+}
+
+getTotalCategoryPages
+mergeJsonFiles
---
+++ cachescripts/fetch-the-standard-posts.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Scrape posts from the standard and store JSON data on disk
+workdir="cachescripts"
+queryPerPage=100
+postsDir="${workdir}/posts-json"
+postsBaseUrl="https://thestandard.co/wp-json/wp/v2/posts"
+postFilename="post"
+outputDir="${postsDir}/merged"
+outputFilename="posts.json"
+cachedDir="src/assets/cached"
+
+getTenPostPages() {
+    echo "Fetching the standard posts"
+    headers=$(curl -I "${postsBaseUrl}?page=1&per_page=${queryPerPage}")
+    rawTotalPosts=$(echo "$headers" | grep -Fi X-WP-Total:)
+    rawTotalPages=$(echo "$headers" | grep -Fi X-WP-TotalPages:)
+    totalPosts=${rawTotalPosts//[!0-9]/}
+    totalPages=${rawTotalPages//[!0-9]/}
+    echo "totalPages: $totalPages"
+    echo "totalPosts: $totalPosts"
+    echo "queryPerPage: $queryPerPage"
+    for ((count = 1; count <= 10; count++)); do
+        if [ $count -le 10 ]; then
+            echo "Fetching the standard posts page: $count"
+            curl -s "${postsBaseUrl}?page=${count}&per_page=${queryPerPage}" \
+                -H 'Accept: application/json' \
+                -H 'Content-Type: application/json' |
+                jq '.' >./${postsDir}/${postFilename}-${count}.json
+        fi
+    done
+}
+mergeJsonFiles() {
+    jq -s 'flatten' ./${postsDir}/${postFilename}*.json >./${outputDir}/${outputFilename}
+    mv ./${outputDir}/${outputFilename} ${cachedDir}/${outputFilename}
+}
+
+getTenPostPages
+mergeJsonFiles
