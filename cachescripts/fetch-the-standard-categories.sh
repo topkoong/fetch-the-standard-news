3ccead24e070662e6762f4095beef783ca7d69f1
toppy <theerut@shoplive.dev>
2022-09-07 20:37:42 +0700

    test curl command

--- cachescripts/fetch-the-standard-categories.sh
+++ cachescripts/fetch-the-standard-categories.sh
@@ -13,6 +13,7 @@ cachedDir="src/assets/cached"
 getTotalCategoryPages() {
     echo "Fetching the standard categories"
     headers=$(curl -I "${categoriesBaseUrl}?page=1&per_page=${queryPerPage}")
+    echo "headers: $headers"
     rawTotalPosts=$(echo "$headers" | grep -Fi X-WP-Total:)
     rawTotalPages=$(echo "$headers" | grep -Fi X-WP-TotalPages:)
     echo "rawTotalPosts: $rawTotalPosts"
@@ -22,25 +23,25 @@ getTotalCategoryPages() {
     echo "totalPages: $totalPages"
     echo "totalPosts: $totalPosts"
     echo "queryPerPage: $queryPerPage"
-    for ((count = 1; count <= $totalPages; count++)); do
-        if [ $count -eq $totalPages ]; then
-            quriedPages=$((count - 1))
-            totalQueriedPages=$((queryPerPage * quriedPages))
-            remainingPosts=$((totalPosts - totalQueriedPages))
-            echo "remainingPosts: $remainingPosts"
-            echo "Fetching the standard categories page: $count"
-            curl -s "${categoriesBaseUrl}?page=${count}&per_page=${remainingPosts}" \
-                -H 'Accept: application/json' \
-                -H 'Content-Type: application/json' |
-                jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
-        else
-            echo "Fetching the standard categories page: $count"
-            curl -s "${categoriesBaseUrl}?page=${count}&per_page=${queryPerPage}" \
-                -H 'Accept: application/json' \
-                -H 'Content-Type: application/json' |
-                jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
-        fi
-    done
+    # for ((count = 1; count <= $totalPages; count++)); do
+    #     if [ $count -eq $totalPages ]; then
+    #         quriedPages=$((count - 1))
+    #         totalQueriedPages=$((queryPerPage * quriedPages))
+    #         remainingPosts=$((totalPosts - totalQueriedPages))
+    #         echo "remainingPosts: $remainingPosts"
+    #         echo "Fetching the standard categories page: $count"
+    #         curl -s "${categoriesBaseUrl}?page=${count}&per_page=${remainingPosts}" \
+    #             -H 'Accept: application/json' \
+    #             -H 'Content-Type: application/json' |
+    #             jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
+    #     else
+    #         echo "Fetching the standard categories page: $count"
+    #         curl -s "${categoriesBaseUrl}?page=${count}&per_page=${queryPerPage}" \
+    #             -H 'Accept: application/json' \
+    #             -H 'Content-Type: application/json' |
+    #             jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
+    #     fi
+    # done
 }
 mergeJsonFiles() {
     jq -s 'flatten' ./${categoriesDir}/${categoriesGroupFilename}-*.json >./${outputDir}/${outputFilename}
@@ -51,4 +52,4 @@ mergeJsonFiles() {
 }
 
 getTotalCategoryPages
-mergeJsonFiles
+# mergeJsonFiles
