3d0c5116c38fe857735ddd565ebd431d631365a2
toppy <theerut@shoplive.dev>
2022-09-07 21:31:24 +0700

    Temporarily disable workflow

--- cachescripts/fetch-the-standard-categories.sh
+++ cachescripts/fetch-the-standard-categories.sh
@@ -12,49 +12,41 @@ cachedDir="src/assets/cached"
 
 getTotalCategoryPages() {
     echo "Fetching the standard categories"
-    # headers=$(curl -I "${categoriesBaseUrl}?page=1&per_page=${queryPerPage}")
-    # echo "headers: $headers"
-    headers=$(curl -I https://jsonplaceholder.typicode.com/posts)
-    echo "headers: $headers"
-    curl -s https://jsonplaceholder.typicode.com/posts \
-        -H 'Accept: application/json' \
-        -H 'Content-Type: application/json' | jq '.'
-    # rawTotalPosts=$(echo "$headers" | grep -Fi X-WP-Total:)
-    # rawTotalPages=$(echo "$headers" | grep -Fi X-WP-TotalPages:)
-    # echo "rawTotalPosts: $rawTotalPosts"
-    # echo "rawTotalPages: $rawTotalPages"
-    # totalPosts=${rawTotalPosts//[!0-9]/}
-    # totalPages=${rawTotalPages//[!0-9]/}
-    # echo "totalPages: $totalPages"
-    # echo "totalPosts: $totalPosts"
-    # echo "queryPerPage: $queryPerPage"
-    # for ((count = 1; count <= $totalPages; count++)); do
-    #     if [ $count -eq $totalPages ]; then
-    #         quriedPages=$((count - 1))
-    #         totalQueriedPages=$((queryPerPage * quriedPages))
-    #         remainingPosts=$((totalPosts - totalQueriedPages))
-    #         echo "remainingPosts: $remainingPosts"
-    #         echo "Fetching the standard categories page: $count"
-    #         curl -s "${categoriesBaseUrl}?page=${count}&per_page=${remainingPosts}" \
-    #             -H 'Accept: application/json' \
-    #             -H 'Content-Type: application/json' |
-    #             jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
-    #     else
-    #         echo "Fetching the standard categories page: $count"
-    #         curl -s "${categoriesBaseUrl}?page=${count}&per_page=${queryPerPage}" \
-    #             -H 'Accept: application/json' \
-    #             -H 'Content-Type: application/json' |
-    #             jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
-    #     fi
-    # done
+    headers=$(curl -I "${categoriesBaseUrl}?page=1&per_page=${queryPerPage}")
+    rawTotalPosts=$(echo "$headers" | grep -Fi X-WP-Total:)
+    rawTotalPages=$(echo "$headers" | grep -Fi X-WP-TotalPages:)
+    totalPosts=${rawTotalPosts//[!0-9]/}
+    totalPages=${rawTotalPages//[!0-9]/}
+    echo "totalPages: $totalPages"
+    echo "totalPosts: $totalPosts"
+    echo "queryPerPage: $queryPerPage"
+    for ((count = 1; count <= $totalPages; count++)); do
+        if [ $count -eq $totalPages ]; then
+            quriedPages=$((count - 1))
+            totalQueriedPages=$((queryPerPage * quriedPages))
+            remainingPosts=$((totalPosts - totalQueriedPages))
+            echo "remainingPosts: $remainingPosts"
+            echo "Fetching the standard categories page: $count"
+            curl -s "${categoriesBaseUrl}?page=${count}&per_page=${remainingPosts}" \
+                -H 'Accept: application/json' \
+                -H 'Content-Type: application/json' |
+                jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
+        else
+            echo "Fetching the standard categories page: $count"
+            curl -s "${categoriesBaseUrl}?page=${count}&per_page=${queryPerPage}" \
+                -H 'Accept: application/json' \
+                -H 'Content-Type: application/json' |
+                jq '.' >./${categoriesDir}/${categoriesGroupFilename}-${count}.json
+        fi
+    done
 }
-# mergeJsonFiles() {
-#     jq -s 'flatten' ./${categoriesDir}/${categoriesGroupFilename}-*.json >./${outputDir}/${outputFilename}
-#     echo "Copying cache file to static folder"
-#     mv ./${outputDir}/${outputFilename} ${cachedDir}/${outputFilename}
-#     echo "Cleaning up cache folder"
-#     rm ./${categoriesDir}/${categoriesGroupFilename}-*.json
-# }
+mergeJsonFiles() {
+    jq -s 'flatten' ./${categoriesDir}/${categoriesGroupFilename}-*.json >./${outputDir}/${outputFilename}
+    echo "Copying cache file to static folder"
+    mv ./${outputDir}/${outputFilename} ${cachedDir}/${outputFilename}
+    echo "Cleaning up cache folder"
+    rm ./${categoriesDir}/${categoriesGroupFilename}-*.json
+}
 
 getTotalCategoryPages
-# mergeJsonFiles
+mergeJsonFiles
