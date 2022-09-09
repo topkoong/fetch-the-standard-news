8f41937cd89d79483dacec3ba2b0c444491d54a1
topkoong <topkoong@users.noreply.github.com>
2022-09-09 15:34:17 +0000

    deploy: 30b90c73723819a0beaca2fd246726e32ddcfc8e

---
+++ assets/PageBreak.9126526a.js
@@ -0,0 +1,1 @@
+import{e}from"./index.e5421068.js";function a(){return e("div",{className:"relative flex py-5 items-center",children:[e("div",{className:"flex-grow border-2 border-white"}),e("div",{className:"flex-grow border-2 border-white"})]})}export{a as default};
